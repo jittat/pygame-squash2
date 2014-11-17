572947d6a5573ce9e403cee557d21f204e7d0238
Jittat Fakcharoenphol <jittat@gmail.com>
2014-11-17 08:18:38 +0700

    updated rendering methods

--- gamelib.py
+++ gamelib.py
@@ -1,22 +1,24 @@
 import pygame
 from pygame.locals import *
 
-class SimpleGame:
+class SimpleGame(object):
 
     def __init__(self,
                  title,
+                 background_color,
                  window_size=(640,480),
                  fps=60):
         self.title = title
         self.window_size = window_size
         self.fps = fps
+        self.background_color = background_color
 
-        self.terminated = False
+        self.is_terminated = False
 
     def __game_init(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.display = pygame.display.set_mode(self.window_size)
+        self.surface = pygame.display.set_mode(self.window_size)
         pygame.display.set_caption(self.title)
         self.font = pygame.font.SysFont("monospace", 20)        
 
@@ -30,15 +32,19 @@ class SimpleGame:
                 self.on_key_up(event.key)
 
     def terminate(self):
-        self.terminated = True
+        self.is_terminated = True
 
     def run(self):
         self.init()
-        while not self.terminated:
+        while not self.is_terminated:
             self.__handle_events()
 
             self.update()
-            self.render()
+
+            self.surface.fill(self.background_color)
+            self.render(self.surface)
+            pygame.display.update()
+
             self.clock.tick(self.fps)
     
     def init(self):
@@ -47,7 +53,7 @@ class SimpleGame:
     def update(self):
         pass
 
-    def render(self):
+    def render(self, surface):
         pass
 
     def on_key_up(self, key):
--- main.py
+++ main.py
@@ -4,15 +4,28 @@ from pygame.locals import *
 import gamelib
 
 class SquashGame(gamelib.SimpleGame):
+    BLACK = pygame.Color('black')
+    WHITE = pygame.Color('white')
+
+    def __init__(self):
+        super(SquashGame, self).__init__('Squash', SquashGame.BLACK)
+        self.x = 0
+
     def on_key_up(self,k):
         print 'UP: ',k
         
     def on_key_down(self,k):
         print 'DOWN: ',k
-        
+
+    def update(self):
+        self.x += 1
+
+    def render(self, surface):
+        pygame.draw.circle(surface, SquashGame.WHITE, (self.x, 100), 10, 0)
+
 
 def main():
-    game = SquashGame('Test')
+    game = SquashGame()
     game.run()
 
 if __name__ == '__main__':
